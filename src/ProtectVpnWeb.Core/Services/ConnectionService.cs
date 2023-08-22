1d0b38d7419415fca9cda67c790d6717e9f37229
F1st3k <N1k0s04k@gmail.com>
2023-08-22 13:01:30 +0300

    refactoring Connection services

--- src/ProtectVpnWeb.Core/Services/ConnectionService.cs
+++ src/ProtectVpnWeb.Core/Services/ConnectionService.cs
@@ -14,9 +14,9 @@ public sealed class ConnectionService<TRepository, TClientConnection, TVpnManage
     where TVpnManager : IVpnService<TClientConnection>
 {
     private TRepository ConnectionRepository { get; }
-    
+
     private TVpnManager VpnManager { get; }
-    
+
     public ConnectionService(
         TRepository connectionRepository,
         TVpnManager vpnManager)
@@ -27,12 +27,12 @@ public sealed class ConnectionService<TRepository, TClientConnection, TVpnManage
 
     public void Restart()
     {
-        if (VpnManager.ServerIsActive) 
+        if (VpnManager.ServerIsActive)
             VpnManager.StopServer();
-        
+
         VpnManager.StartServer();
-        
-        var connections = 
+
+        var connections =
             ConnectionRepository.GetRange(0, ConnectionRepository.Count);
         foreach (var c in connections)
         {
@@ -44,15 +44,9 @@ public sealed class ConnectionService<TRepository, TClientConnection, TVpnManage
 
     public ConnectionDto GetConnection(int id)
     {
-        if (id < 0)
-            throw new InvalidArgumentException(
-                new ExceptionParameter(id, nameof(id)));
-
-        if (ConnectionRepository.CheckIdUniqueness(id))
+        if (TryGetConnection(id, out var connection) == false || connection is null)
             throw new NotFoundException(
                 new ExceptionParameter(id, nameof(id)));
-
-        var connection = ConnectionRepository.GetById(id);
 
         return connection.ToTransfer();
     }
@@ -66,32 +60,21 @@ public sealed class ConnectionService<TRepository, TClientConnection, TVpnManage
 
         if (startIndex + count > ConnectionRepository.Count)
             throw new RangeException(
-                new ExceptionParameter(startIndex+count, 
+                new ExceptionParameter(startIndex + count,
                     nameof(startIndex) + "+" + nameof(count)),
-                new ExceptionParameter(ConnectionRepository.Count, 
+                new ExceptionParameter(ConnectionRepository.Count,
                     nameof(ConnectionRepository.Count)));
 
         var connections = ConnectionRepository.GetRange(startIndex, count);
-        
-        var connectionsDto = new ConnectionDto[connections.Length];
-        for (int i = 0; i < connections.Length; i++)
-        {
-            connectionsDto[i] = connections[i].ToTransfer();
-        }
-        return connectionsDto;
+        return Array.ConvertAll(connections, input => input.ToTransfer());
     }
 
     public UserDto GetUserByConnection(int id)
     {
-        if (id < 0)
-            throw new InvalidArgumentException(
-                new ExceptionParameter(id, nameof(id)));
-
-        if (ConnectionRepository.CheckIdUniqueness(id))
+        if (TryGetConnection(id, out var connection) == false || connection is null)
             throw new NotFoundException(
                 new ExceptionParameter(id, nameof(id)));
-        
-        var connection = ConnectionRepository.GetById(id);
+
         var user = ConnectionRepository.GetRelatedEntity(connection);
         return user.ToTransfer();
     }
@@ -100,27 +83,28 @@ public sealed class ConnectionService<TRepository, TClientConnection, TVpnManage
     {
         if (dto.UserId < 0)
             throw new InvalidArgumentException(
-            new ExceptionParameter(dto.UserId, nameof(dto.UserId)));
-        
+                new ExceptionParameter(dto.UserId, nameof(dto.UserId)));
+
         if (VpnManager.ServerIsActive == false)
             throw new NotRunningException(nameof(VpnManager));
 
-        string info = VpnManager.GenerateConnectionInfo();
+        var info = VpnManager.GenerateConnectionInfo();
         var newConnection = new Connection(ConnectionRepository.GetNextId(), dto.UserId, info);
         ConnectionRepository.Add(newConnection);
-        
+
         var newClientConnection = new TClientConnection();
         newClientConnection.ChangeOf(newConnection);
         VpnManager.AddConnection(newClientConnection);
-        
+
         return newConnection.ToTransfer();
     }
 
     public void EditConnection(int id, ConnectionDto dto)
     {
-        if (id < 0)
-            throw new InvalidArgumentException(
+        if (TryGetConnection(id, out var connection) == false || connection is null)
+            throw new NotFoundException(
                 new ExceptionParameter(id, nameof(id)));
+
         if (dto.UserId < 0 ||
             dto.Id < 0 ||
             dto.Info == string.Empty)
@@ -129,20 +113,14 @@ public sealed class ConnectionService<TRepository, TClientConnection, TVpnManage
                 new ExceptionParameter(dto.Id, nameof(dto.Id)),
                 new ExceptionParameter(dto.Info, nameof(dto.Info)));
 
-        if (ConnectionRepository.CheckIdUniqueness(id))
-            throw new NotFoundException(
-            new ExceptionParameter(id, nameof(id)));
-
-        var connection = ConnectionRepository.GetById(id);
-
         if (connection.Id != dto.Id)
             throw new NonIdenticalException(
                 new ExceptionParameter(connection.Id, nameof(connection.Id)),
-                    new ExceptionParameter(dto.Id, nameof(dto.Id)));
-        
+                new ExceptionParameter(dto.Id, nameof(dto.Id)));
+
         connection.ChangeOf(dto);
-        ConnectionRepository.Update(connection); 
-        
+        ConnectionRepository.Update(connection);
+
         var clientConnection = new TClientConnection();
         clientConnection.ChangeOf(connection);
         VpnManager.UpdateConnection(clientConnection);
@@ -150,20 +128,28 @@ public sealed class ConnectionService<TRepository, TClientConnection, TVpnManage
 
     public void RemoveConnection(int id)
     {
-        if (id < 0)
-            throw new InvalidArgumentException(
-                new ExceptionParameter(id, nameof(id)));
-
-        if (ConnectionRepository.CheckIdUniqueness(id))
+        if (TryGetConnection(id, out var connection) == false || connection is null)
             throw new NotFoundException(
                 new ExceptionParameter(id, nameof(id)));
-
-        var connection = ConnectionRepository.GetById(id);
         
         ConnectionRepository.Remove(connection.Id);
         
         var clientConnection = new TClientConnection();
         clientConnection.ChangeOf(connection);
         VpnManager.RemoveConnection(clientConnection);
+    }
+
+    private bool TryGetConnection(int id, out Connection? connection)
+    {
+        connection = null;
+        if (id < 0)
+            throw new InvalidArgumentException(
+                new ExceptionParameter(id, nameof(id)));
+
+        if (ConnectionRepository.CheckIdUniqueness(id))
+            return false;
+
+        connection = ConnectionRepository.GetById(id);
+        return true;
     }
 }
