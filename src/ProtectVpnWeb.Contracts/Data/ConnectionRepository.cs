1b883e2973ade59c5c362c3d93acdd274e709671
F1st3K <n1k0s04k@gmail.com>
2023-10-03 18:38:20 +0300

    create connection repository

--- src/ProtectVpnWeb.Contracts/Data/ConnectionRepository.cs
+++ src/ProtectVpnWeb.Contracts/Data/ConnectionRepository.cs
@@ -1,49 +1,80 @@
+using ProtectVpnWeb.Contracts.Data.Mappers;
 using ProtectVpnWeb.Core.Entities;
 using ProtectVpnWeb.Core.Repositories;
+using ProtectVpnWeb.Data;
+using ProtectVpnWeb.Data.Entities;
 
 namespace ProtectVpnWeb.Contracts.Data;
 
 public class ConnectionRepository : IRepository<Connection>,
     IOneRelationshipRepository<Connection, User>
 {
-    public int Count { get; }
+    private readonly IMapper<User, UserEntity> _userMapper = new UserMapper();
+    private readonly IMapper<Connection, ConnectionEntity> _connectionMapper = new ConnectionMapper();
+    
+    public int Count
+    {
+        get
+        {
+            using var dbContext = new DataContext();
+            return dbContext.Connections.Count();
+        }
+    }
+
     public int GetNextId()
     {
-        throw new NotImplementedException();
+        using var dbContext = new DataContext();
+        return dbContext.Connections.Max(connection => connection.Id) + 1;
     }
 
     public bool CheckIdUniqueness(int id)
     {
-        throw new NotImplementedException();
+        using var dbContext = new DataContext();
+        return !dbContext.Connections.Any(connection => connection.Id == id);
     }
 
     public void Add(Connection entity)
     {
-        throw new NotImplementedException();
+        var connection = _connectionMapper.ToData(entity);
+        using var dbContext = new DataContext();
+        dbContext.Connections.Add(connection);
+        dbContext.SaveChanges();
     }
 
     public Connection GetById(int id)
     {
-        throw new NotImplementedException();
+        using var dbContext = new DataContext();
+        var connection = dbContext.Connections.First(connection => connection.Id == id);
+        return _connectionMapper.ToDomain(connection);
     }
 
     public Connection[] GetRange(int index, int count)
     {
-        throw new NotImplementedException();
+        using var dbContext = new DataContext();
+        var connections = dbContext.Connections.Skip(index).Take(count).ToList();
+        return connections.ConvertAll(connection => _connectionMapper.ToDomain(connection)).ToArray();
     }
 
     public void Update(Connection entity)
     {
-        throw new NotImplementedException();
+        var connection = _connectionMapper.ToData(entity);
+        using var dbContext = new DataContext();
+        dbContext.Connections.Update(connection);
+        dbContext.SaveChanges();
     }
 
     public void Remove(int id)
     {
-        throw new NotImplementedException();
+        using var dbContext = new DataContext();
+        dbContext.Connections.Remove(dbContext.Connections.First(connection => connection.Id == id));
+        dbContext.SaveChanges();
     }
 
     public User GetRelatedEntity(Connection source)
     {
-        throw new NotImplementedException();
+        using var dbContext = new DataContext();
+        var connection = _connectionMapper.ToData(source);
+        var user = dbContext.Users.First(user => user.Id == connection.UserId);
+        return _userMapper.ToDomain(user);
     }
 }
